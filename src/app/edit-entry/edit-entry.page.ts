a6db287f3e73f45050590bbd52750cceb0dff045
lisaokezie <56400443+lisaokezie@users.noreply.github.com>
2020-01-11 00:40:20 +0100

    Einträge lassen sich bearbeiten

--- src/app/edit-entry/edit-entry.page.ts
+++ src/app/edit-entry/edit-entry.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+
+import { EntriesService } from '../services/entries.service';
+import { Entry } from '../interfaces/entry';
 
 @Component({
   selector: 'app-edit-entry',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEntryPage implements OnInit {
 
-  constructor() { }
+  public entry: Entry;
+
+  constructor(private route: ActivatedRoute, private entriesService: EntriesService, private navCtrl: NavController) {
+
+    this.entry = {
+      id: '',
+      title: '',
+      date: new Date(),
+      content: '',
+      isFavorite: false
+    }
+
+   }
 
   ngOnInit() {
+    let entryId = this.route.snapshot.paramMap.get('id');
+
+    if(this.entriesService.loaded){
+      this.entry = this.entriesService.getEntry(entryId);
+    } else{
+      this.entriesService.load().then(() => {
+        this.entry = this.entriesService.getEntry(entryId)
+      });
+    }
   }
+
+  save(){
+    this.entriesService.save();
+  }
+
 
 }
